2f25ef419d5a9dbbe99d25eff3205f1f109cc009
szl6038 <szl6038@psu.edu>
2020-09-16 23:23:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Author: Shang Yuan Lim szl6038@psu.edu
+
+def digit_sum(n):
+  if n <= 9:
+    return n
+  else:
+    return n%10 + digit_sum(n//10)
+    
+    
+def run():
+  value = input("Enter an int: ")
+
+  value = int(value)
+
+  n = digit_sum(value)
+
+  print(f"sum of digits of {value} is " + str(n))
+
+if __name__ == "__main__":
+  run ()
+
